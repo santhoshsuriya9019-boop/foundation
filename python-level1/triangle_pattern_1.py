5cf65055ab2150e5cdb97819e2f155f3b28332ac
santhoshsuriya9019 <santhoshsuriya9019@gmail.com>
2026-06-28 19:55:15 +0530

    Python - few progs and starting in java

---
+++ python-level1/triangle_pattern_1.py
@@ -0,0 +1,8 @@
+a= int(input("Enter a numberr:"))
+
+for i in range(1, a+1):
+    print()
+    for j in range(1, i+1):
+        print(j, end=" ")    # trinagle pattern model 1
+        
+        
---
+++ src/TrianglePatternPalindrome.java
@@ -0,0 +1,14 @@
+
+public class TrianglePatternPalindrome {
+
+	public static void printTrianglePalindrome(int n) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		printTrianglePalindrome(5);
+	}
+
+}
